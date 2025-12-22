77e221cbb50702cd59a1a9c41e2bbeb335cff3ed
aditya <adityasingh.sss998@gmail.com>
2025-12-22 22:32:55 +0530

    added password hashing and login logic

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 router.post('/register', async (req, res) => {
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(req.body.pass, salt);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    pass: req.body.pass,
+    pass: hash,
     org: req.body.org
   });
 
@@ -16,5 +20,16 @@ router.post('/register', async (req, res) => {
     res.status(400).send(err);
   }
 });
+const jwt = require('jsonwebtoken');
 
+router.post('/login', async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('Wrong Email');
+
+  const validPass = await bcrypt.compare(req.body.pass, user.pass);
+  if (!validPass) return res.status(400).send('Wrong Pass');
+
+  const token = jwt.sign({ id: user._id }, process.env.KEY);
+  res.header('auth', token).send(token);
+});
 module.exports = router;
